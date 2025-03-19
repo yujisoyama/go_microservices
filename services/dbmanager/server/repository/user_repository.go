68083c3cc1d117533aaaded435d7adc2450ace04
Fabio Soyama <fabio.soyama@4all.com>
2025-03-19 19:20:56 -0300

    :sparkles: Add transaction

--- services/dbmanager/server/repository/user_repository.go
+++ services/dbmanager/server/repository/user_repository.go
@@ -19,61 +19,76 @@ const (
 )
 
 func UpsertUser(ctx context.Context, dbClient *mongo.Client, user *dbmanager.UpsertUserRequest) (*dbmanager.UpsertUserResponse, error) {
-	collection := dbClient.Database(DB).Collection(COLLECTION)
+	session, err := dbClient.StartSession()
+	if err != nil {
+		return nil, fmt.Errorf("Error in starting session: %v", err)
+	}
+	defer session.EndSession(ctx)
+
 	resp := &dbmanager.UpsertUserResponse{}
 
-	var usr entity.UserEntity
-	err := collection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&usr)
-	if err == mongo.ErrNoDocuments {
-		newUser := &entity.UserEntity{
-			Name:      user.Name,
-			Email:     user.Email,
-			CreatedAt: time.Now(),
-			UpdatedAt: time.Now(),
+	callback := func(sessCtx mongo.SessionContext) (interface{}, error) {
+		collection := dbClient.Database(DB).Collection(COLLECTION)
+
+		var usr entity.UserEntity
+		err = collection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&usr)
+		if err == mongo.ErrNoDocuments {
+			newUser := &entity.UserEntity{
+				Name:      user.Name,
+				Email:     user.Email,
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+			}
+
+			res, err := collection.InsertOne(ctx, newUser)
+			if err != nil {
+				return nil, fmt.Errorf("Error in insert user: %v", err)
+			}
+
+			oId, ok := res.InsertedID.(primitive.ObjectID)
+			if !ok {
+				return nil, fmt.Errorf("Error in converting ID of insert user: %v", err)
+			}
+
+			resp.User = &userpb.User{
+				Id:        oId.Hex(),
+				Name:      newUser.Name,
+				Email:     newUser.Email,
+				CreatedAt: newUser.CreatedAt.UTC().Format(time.RFC3339),
+				UpdatedAt: newUser.UpdatedAt.UTC().Format(time.RFC3339),
+			}
+			resp.User.Id = oId.Hex()
+		} else {
+			upUser := &entity.UserEntity{
+				Name:      user.Name,
+				Email:     user.Email,
+				CreatedAt: usr.CreatedAt,
+				UpdatedAt: time.Now(),
+			}
+
+			update := bson.M{
+				"$set": upUser,
+			}
+
+			_, err = collection.UpdateOne(ctx, bson.M{"_id": usr.ID}, update)
+			if err != nil {
+				return nil, fmt.Errorf("Error in updating user: %v", err)
+			}
+
+			resp.User = &userpb.User{
+				Id:        usr.ID.Hex(),
+				Name:      user.Name,
+				Email:     user.Email,
+				CreatedAt: usr.CreatedAt.UTC().Format(time.RFC3339),
+				UpdatedAt: upUser.UpdatedAt.UTC().Format(time.RFC3339),
+			}
 		}
 
-		res, err := collection.InsertOne(ctx, newUser)
-		if err != nil {
-			return nil, fmt.Errorf("Error in insert user: %v", err)
-		}
-
-		oId, ok := res.InsertedID.(primitive.ObjectID)
-		if !ok {
-			return nil, fmt.Errorf("Error in converting ID of insert user: %v", err)
-		}
-
-		resp.User = &userpb.User{
-			Id:        oId.Hex(),
-			Name:      newUser.Name,
-			Email:     newUser.Email,
-			CreatedAt: newUser.CreatedAt.UTC().Format(time.RFC3339),
-			UpdatedAt: newUser.UpdatedAt.UTC().Format(time.RFC3339),
-		}
-		resp.User.Id = oId.Hex()
-	} else {
-		upUser := &entity.UserEntity{
-			Name:      user.Name,
-			Email:     user.Email,
-			CreatedAt: usr.CreatedAt,
-			UpdatedAt: time.Now(),
-		}
-
-		update := bson.M{
-			"$set": upUser,
-		}
-
-		_, err = collection.UpdateOne(ctx, bson.M{"_id": usr.ID}, update)
-		if err != nil {
-			return nil, fmt.Errorf("Error in updating user: %v", err)
-		}
-
-		resp.User = &userpb.User{
-			Id:        usr.ID.Hex(),
-			Name:      user.Name,
-			Email:     user.Email,
-			CreatedAt: usr.CreatedAt.UTC().Format(time.RFC3339),
-			UpdatedAt: upUser.UpdatedAt.UTC().Format(time.RFC3339),
-		}
+		return nil, nil
+	}
+	_, err = session.WithTransaction(ctx, callback)
+	if err != nil {
+		return nil, fmt.Errorf("Error in transaction: %v", err)
 	}
 
 	return resp, nil
